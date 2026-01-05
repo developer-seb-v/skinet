cc27b1fe525f09bc2d5e9c32443963f4cd526c04
Sebastian Valenzuela <developer.seb.v@gmail.com>
2026-01-05 06:13:35 -0800

    create product repository and implementation in controller

--- Core/obj/Debug/net8.0/Core.AssemblyInfo.cs
+++ Core/obj/Debug/net8.0/Core.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Core")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+847e506b180d58314756188cb92769ef83c759b5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+504876a60614e22ffba089acb6f5714cdb363a27")]
 [assembly: System.Reflection.AssemblyProductAttribute("Core")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Core")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
